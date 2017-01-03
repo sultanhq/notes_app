d7edfc6c2c84b7c8db0910e8dc57052428558ad2
Rob Adams <robert.andrew.adams@gmail.com>
2017-01-03 11:52:45 +0000

    added two tests for note and message existence

--- tests/noteSpec.js
+++ tests/noteSpec.js
@@ -1,9 +1,34 @@
 // test specs
 //test for the initialization of the notes object
 
-function noteClassDefined () {
-  if (typeof(Note) === "undefined" ) {
+function noteClassDefined() {
+  if (typeof(Note) === "undefined") {
     throw new Error("Note is not defined");
+  } else {
+    console.log("noteClassDefined test passed");
   }
 }
 noteClassDefined();
+
+function noteIsTypeOfNote() {
+  var note = new Note();
+  if (note instanceof Note === false) {
+    throw new Error("Note is not defined");
+  } else {
+    console.log("noteIsTypeOfNote test passed");
+  }
+}
+
+noteIsTypeOfNote();
+
+
+function noteMessageExistsUponCreation() {
+  var note = new Note("Hello World");
+  if (note.message !== "Hello World") {
+    throw new Error("Note message does not exist");
+  } else {
+    console.log("noteMessageExistsUponCreation test passed");
+  }
+}
+
+noteMessageExistsUponCreation();
