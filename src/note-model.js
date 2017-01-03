0c0ba1fb64fcdaa48e312dade32163fe4c8d3dbb
TudorTacal <tudortacal@gmail.com>
2017-01-03 11:33:52 +0000

    Wrote the first test and passed for the Note class

---
+++ src/note-model.js
@@ -0,0 +1,1 @@
+var Note = function () {};
--- tests/noteSpec.js
+++ tests/noteSpec.js
@@ -1,1 +1,9 @@
 // test specs
+//test for the initialization of the notes object
+
+function noteClassDefined () {
+  if (typeof(Note) === "undefined" ) {
+    throw new Error("Note is not defined");
+  }
+}
+noteClassDefined();
