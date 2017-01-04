b6a1de6740505cd19fcbb9ee31ea54de7336ecc0
Rob Adams <robert.andrew.adams@gmail.com>
2017-01-04 10:02:40 +0000

    created index.js using spike

--- src/models/note-controller.js
+++ src/models/note-controller.js
@@ -1,7 +1,3 @@
-var noteList = new List();
-
-createNote(noteList,"I like Nutella");
-
 (function changeDiv () {
   var elem = document.getElementById('app');
   elem.innerHTML = htmlList(noteList);
