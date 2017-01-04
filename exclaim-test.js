941ed9ff3bfe61657b49bc128dc16fb1edf39647
Russell Yeo <rusyeo@gmail.com>
2017-01-04 11:31:08 +0000

    update index.html and package.json for http-server

--- exclaim-test.js
+++
@@ -1,7 +0,0 @@
-var exclaim = require("./exclaim").exclaim;
-
-if (exclaim("hi") !== "hi!!!!!") {
-  throw new Error("Exclaiming hi should equal hi!!!!!");
-} else {
-  console.log("Yo Teako");
-}
--- exclaim.js
+++
@@ -1,9 +0,0 @@
-(function(exports) {
-  var EXCLAMATION_MARK_COUNT = 5
-
-  function exclaim(string) {
-    return string + "!".repeat(EXCLAMATION_MARK_COUNT);
-  };
-
-  exports.exclaim = exclaim;
-})(this);
