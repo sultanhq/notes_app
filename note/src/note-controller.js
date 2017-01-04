5421cf82891d47c74911c624c29e9f81e6ee35a4
Russell Yeo <rusyeo@gmail.com>
2017-01-04 13:27:31 +0000

    spooky

--- note/src/note-controller.js
+++ note/src/note-controller.js
@@ -1,3 +1,18 @@
-function returnHowdy() {
-  return "Howdy";
-}
+(function(exports) {
+
+  function NoteController(noteList){
+    this.noteList = noteList;
+    var note = new Note("favourite drink: beer");
+    noteList.saveNote(note);
+    var view = new NoteListView(noteList);
+  };
+
+  NoteController.prototype.insertHTML = function(){
+    var html = view.displayHTML();
+    var element = document.getElementById("app");
+    element.innerHTML = html;
+  };
+
+  exports.NoteController = NoteController;
+
+})(this);
