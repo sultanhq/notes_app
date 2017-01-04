6b3c68fc491bca2eb212bbf06718ab41d6a37e46
DarkChocLatte <darkchoclatte@gmail.com>
2017-01-04 11:09:28 +0000

    displayHTML added

--- note/spec/note-list-view-test.js
+++ note/spec/note-list-view-test.js
@@ -7,4 +7,17 @@ function testNewNoteListViewCreated() {
   console.log("This returned: " + noteListView.seeNoteList())
 };
 
+function testNewNoteListViewCreated() {
+  var note1        = new Note("Yolo");
+  var note2        = new Note("Yoko");
+  var noteList     = new NoteList();
+  noteList.saveNote(note1);
+  noteList.saveNote(note2);
+  var noteListView = new NoteListView(noteList);
+  var html         = "<ul><li><div>Yolo</div></li><li><div>Yoko</div></li></ul>";
+  assert.isTrue(noteListView.displayHTML() === html );
+  console.log("This returned: " + noteListView.displayHTML());
+};
+
+
 testNewNoteListViewCreated();
--- note/src/note-list-view.js
+++ note/src/note-list-view.js
@@ -10,6 +10,18 @@
     return this._noteList;
   };
 
+  NoteListView.prototype.displayHTML = function() {
+    var allNotes = this._noteList.seeList();
+    var string   = "<ul>";
+    for (var i = 0; i < allNotes.length; i++ ) {
+      string += ("<li><div>" + allNotes[i].readNote() + "</div></li>");
+    }
+    string += "</ul>";
+    return string;
+
+  }
+
+
   exports.NoteListView = NoteListView;
 
 })(this);
