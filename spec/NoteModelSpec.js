42f0fb10ec7e697044270e4f59225cddef60c76f
keomony khun <keomony@gmail.com>
2017-01-05 15:00:53 +0000

    no change to the commit

--- spec/NoteModelSpec.js
+++ spec/NoteModelSpec.js
@@ -1,13 +1,12 @@
 "use strict";
 
-( function(windowArg) {
-
+(function(windowArg) {
 
 function testNoteModel(){
   var text = "Hello, world";
   var note = new Note(text);
   if (note.getText() !== "Hello, world") {
-    throw new Error("The text wasn't saved")
+    throw new Error("The text wasn't saved");
   }
   else {console.log("Note function pass")}
 };
