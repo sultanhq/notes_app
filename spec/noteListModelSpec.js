5e8e2bae0e9eb97d42bea6ea67e666a3ba4d5259
Agata Sumowska <agata.sumowska@gmail.com>
2017-01-05 16:11:14 +0000

    getAllNotes method

--- spec/noteListModelSpec.js
+++ spec/noteListModelSpec.js
@@ -4,7 +4,7 @@
   var noteList = new NoteList();
   noteList.addNote("Hello, world");
 
-  if(noteList.notesArr[0].getText() !== "Hello, world") {
+  if(noteList.getAllNotes()[0].getText() !== "Hello, world") {
     throw new Error("Failed: The note wasn't saved");
   } else {
     console.log("Passed: The note is saved");
@@ -16,7 +16,7 @@
   noteList.addNote("Hello, world");
   noteList.addNote("Favorite curry: Amok");
 
-  if(noteList.getAllNotes().includes(noteList.notes[0])){
+  if(noteList.getAllNotes().includes(noteList.getAllNotes()[0])){
     console.log("Passed: It returns all the note models stored in the array.");
   } else {
     throw new Error("Failed: It doesn't return all the note models stored in the array.");
--- src/noteListModel.js
+++ src/noteListModel.js
@@ -8,5 +8,11 @@ NoteList.prototype.addNote = function(text) {
   var newNote = new Note(text);
   this.notesArr.push(newNote);
 };
+
+NoteList.prototype.getAllNotes = function(){
+  return this.notesArr;
+
+};
+
 windowArg.NoteList = NoteList;
 })(this);
